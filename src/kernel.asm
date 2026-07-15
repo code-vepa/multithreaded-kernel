edc6bc62527fd3cf8dc80367b4bdcb69698100b1
Vepa <vepa@pdx.edu>
2026-07-15 16:22:43 -0700

    ADDED: IO folder, io.asm and io.c for IO functions

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,7 +1,6 @@
 [BITS 32] ; All code underneath is seen as 32 bits
 
 global _start
-global test_problem
 
 extern kernel_main
 
@@ -26,10 +25,5 @@ _start:
 
     call kernel_main
     jmp $
-
-test_problem:
-    mov eax, 0
-    div eax
-
 
 times 512-($ - $$) db 0
--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "idt/idt.h"
+#include "io/io.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -61,7 +62,6 @@ void print(const char* str){
     }
 }
 
-extern void test_problem();
 
 void kernel_main(void){
 
@@ -72,5 +72,7 @@ void kernel_main(void){
     //initialize the IDT
     idt_init();
 
-    test_problem();
+    //test
+    outb(0x60, 0xff);
+
 }
