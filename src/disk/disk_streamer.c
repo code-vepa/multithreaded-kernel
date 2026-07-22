12f365c2fc1a9aa3f709947b86488c2107921a6e
Vepa <vepa@pdx.edu>
2026-07-22 14:18:21 -0700

    UPDATE: created a disk streamer to read bytes from the disk to the buffer

---
+++ src/disk/disk_streamer.c
@@ -0,0 +1,66 @@
+#include "disk_streamer.h"
+#include "memory/heap/kernel_heap.h"
+#include "config.h"
+
+/*
+    @name: diskstreamer_new
+    @brief: creates a new disk streamer
+            sets byte position to 0
+    @param: int disk id
+    @return: pointer to new created streamer (disk_stream*)
+*/
+
+disk_stream_t* diskstreamer_new(int disk_id){
+    disk_t* disk = disk_get(disk_id);
+    if(!disk){
+        return NULL;
+    }
+
+    disk_stream_t* streamer = kzalloc(sizeof(disk_stream_t));
+
+    streamer->pos = 0;
+    streamer->disk = disk;
+    return streamer;
+}
+
+/*
+    @name: diskstreamer_seek
+    @brief: changes the position of the disk streamer to provided position
+    @param: streamer (disk_stream_t* type), int position (provided position)
+    @return: int 0
+*/
+int diskstreamer_seek(disk_stream_t* streamer, int pos){
+    streamer->pos = pos;
+    return 0;
+}
+
+int diskstreamer_read(disk_stream_t* stream, void* out, int total){
+    int sector = stream->pos / VARGOOS_SECTOR_SIZE;
+    int offset = stream->pos % VARGOOS_SECTOR_SIZE;
+    char buffer[VARGOOS_SECTOR_SIZE];
+
+    int response = disk_read_block(stream->disk, sector, 1, buffer);
+
+    if(response < 0){
+        goto out;
+    }
+
+    int total_to_read = total > VARGOOS_SECTOR_SIZE ? VARGOOS_SECTOR_SIZE : total;
+    
+    for(int i = 0; i < total_to_read; ++i){
+        *(char*)out++ = buffer[offset + i];
+    }
+
+    stream->pos += total_to_read;
+    if(total > VARGOOS_SECTOR_SIZE){
+        response = diskstreamer_read(stream, out, total - VARGOOS_SECTOR_SIZE);
+    }
+
+out:
+    return response;
+}
+
+void diskstreamer_close(disk_stream_t* stream){
+    kfree(stream);
+};
+
---
+++ src/disk/disk_streamer.h
@@ -0,0 +1,16 @@
+#ifndef DISK_STREAMER_H
+#define DISK_STREAMER_H
+
+#include "disk.h"
+
+struct disk_stream{
+    int pos; //byte position
+    disk_t* disk;
+} typedef disk_stream_t;
+
+disk_stream_t* diskstreamer_new(int disk_id);
+int diskstreamer_seek(disk_stream_t* streamer, int pos);
+int diskstreamer_read(disk_stream_t* stream, void* out, int total);
+void diskstreamer_close(disk_stream_t* stream);
+
+#endif
