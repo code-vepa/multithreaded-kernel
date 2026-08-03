056440f3303356d2718ee1392543b02988131672
Vepa <vepa@pdx.edu>
2026-08-03 15:36:38 -0700

    FIXED: added a panic function to handle unrecoverable issues

--- src/kernel.c
+++ src/kernel.c
@@ -61,9 +61,16 @@ void print(const char* str){
 
 static paging_4gb_chunk* kernel_chunk = 0;
 
+void panic(const char* message){
+    print(message);
+
+    while(1){}
+}
+
 void kernel_main(void){
 
     terminal_initialize();
+    
     // first byte (03) is the color and second byte is (41) is the char
     print("Hello world\ntest");
 
