5555575c97521c6dbb848a7722be0f0b29d2c2de
Vepa <vepa@pdx.edu>
2026-08-03 14:31:13 -0700

    UPDATE: implemented VFS fstat function

--- src/filesystem/file.h
+++ src/filesystem/file.h
@@ -14,18 +14,26 @@ enum{
     FILE_MODE_READ, FILE_MODE_WRITE, FILE_MODE_APPEND, FILE_MODE_INVALID
 };
 
+typedef unsigned int FILE_STAT_FLAGS;
+enum{
+    FILE_STAT_READ_ONLY = 0b00000001
+};
+
 typedef struct disk disk_t;
+struct file_stat;
 
 typedef void*(*FS_OPEN_FUNCTION)(disk_t* disk, path_part_t* part, FILE_MODE mode);
 typedef int(*FS_READ_FUNCTION)(disk_t* disk, void* private, uint32_t size, uint32_t nmemb, char* out);
 typedef int(*FS_RESOLVE_FUNCTION)(disk_t* disk);
 typedef int(*FS_SEEK_FUNCTION)(void* private, uint32_t offset, FILE_SEEK_MODE mode);
+typedef int(*FS_STAT_FUNCTION)(disk_t* disk, void* private, struct file_stat* stat);
 
 typedef struct filesystem{
     FS_RESOLVE_FUNCTION resolve;
     FS_OPEN_FUNCTION open;
     FS_READ_FUNCTION read;
     FS_SEEK_FUNCTION seek;
+    FS_STAT_FUNCTION stat;
     char name[20];
 } filesystem_t;
 
@@ -36,10 +44,17 @@ typedef struct file_descriptor{
     disk_t* disk; //the disk that filesystem will be used on
 } file_descriptor_t;
 
+struct file_stat{
+    FILE_STAT_FLAGS flags;
+    uint32_t filesize;
+};
+
+
 void fs_init();
 int fopen(const char* filename, const char* mode_string);
 int fread(void* ptr, uint32_t size, uint32_t nmemb, int fd);
 int fseek(int fd, int offset, FILE_SEEK_MODE mode);
+int fstat(int fd, struct file_stat* stat);
 void fs_insert_filesystem(filesystem_t* filesystem);
 filesystem_t* fs_resolve(disk_t* disk);
 
